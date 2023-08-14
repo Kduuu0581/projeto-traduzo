4d1d16139d4a11c1cabc8e1aa15a8fcfa3c72a1f
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-08-14 16:12:45 -0300

    4 - CONTROLLER & VIEW - Endpoint Tradutor, renderizando variáveis do Backend - GET

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
 # from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
+from models.language_model import LanguageModel
+
 # from models.history_model import HistoryModel
 
 
@@ -10,7 +12,26 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    languages = LanguageModel.list_dicts()
+    translated = "Tradução"
+    print("languages: ", languages)
+    if request.method == "POST":
+        text_to_translate = request.form["text-to-translate"]
+        translate_from = request.form["translate-from"]
+        translate_to = request.form["translate-to"]
+
+        return render_template(
+            "index.html",
+            languages=languages,
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
+        )
+
+    return render_template(
+        "index.html", languages=languages, translated=translated
+    )
 
 
 # Req. 6
